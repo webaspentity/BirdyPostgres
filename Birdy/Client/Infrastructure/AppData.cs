c31e2d8b716fe0c7eca2cd57478f519983edf395
Сергей Евсеев <webaspentity@outlook.com>
2023-03-13 00:24:59 +0300

    13.03.23 0:24

--- Birdy/Client/Infrastructure/AppData.cs
+++ Birdy/Client/Infrastructure/AppData.cs
@@ -2,8 +2,8 @@
 
 public static class AppData
 {
-    public static string BaseUri { get; set; } = "https://localhost:7059";
+    //public static string BaseUri { get; set; } = "https://localhost:7059";
     //public static string BaseUri { get; set; } = "https://95.165.96.3";
-    //public static string BaseUri { get; set; } = "https://evseev.somee.com";
+    public static string BaseUri { get; set; } = "https://evseev.somee.com";
     //public static string BaseUri { get; set; } = "http://evseev.42web.io";
 }
